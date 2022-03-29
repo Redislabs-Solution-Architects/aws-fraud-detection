c03f253c1d367df788bb136a94e05ccf9dc5cb0d
Srini Pendyala <spendyaala@gmail.com>
2022-03-29 08:54:19 -0500

    Adding Grafana terraform file.

---
+++ terraform/grafana.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+    }
+  }
+}
+
+provider "grafana" {
+  url  = "http://localhost:3000/"
+  auth = "admin:admin"
+}
+
+# Got the type by running:
+# curl -s -H "Authorization: Basic YWRtaW46YWRtaW4=" http://localhost:3000/api/datasources |jq
+
+resource "grafana_data_source" "redis-enterprise-cloud" {
+  type       = "redis-datasource"
+  name       = "RedisEnterpriseCloud"
+  url        = "redis-14445.c18475.us-west-2-mz.ec2.cloud.rlrcp.com:14445"
+  is_default = true
+}
+
+# Grab dasboard
+# Need to be sure and export by clicking on the share item - totally messed up
+
+resource "grafana_dashboard" "frauds_scoring" {
+  config_json = file("grafana-dashboard.json")
+}
